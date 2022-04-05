9575a392a392813e6fdb6354fbea12601b6be8a5
KatrinaReid <102809938+KatrinaReid@users.noreply.github.com>
2022-04-05 19:04:10 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+prompt("On a scale of 1-10 how much of a Techii Lady are you?");
+var a = 10;
+if (a > 7 ) {
+  alert("Welcome to Techii Ladies!");
+} else {
+  alert("Want to join Techii Ladies?");
+}
